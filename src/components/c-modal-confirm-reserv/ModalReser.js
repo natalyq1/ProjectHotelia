cf3b7dc54038da88e8c6ab62e6eb17dfa0d9b3db
natalyq1 <onatylili@gmail.com>
2022-08-02 21:10:41 -0500

    Modal reserv arranged with info

--- src/components/c-modal-confirm-reserv/ModalReser.js
+++ src/components/c-modal-confirm-reserv/ModalReser.js
@@ -54,51 +54,51 @@ function ModalConfirmReserv({ index, reservas, habitaciones, users, setUplist, u
                     <div class="rendered-form">
                         <div class="formbuilder-text form-group field-usuario">
                             <label for="usuario" class="formbuilder-text-label">Usuario:</label>
-                            <input type="text" placeholder="nombre usuario{reservas.valornoche}" class="form-control" name="usuario" access="false" id="usuario"></input>
+                            <input type="text" placeholder="Luis gomez" class="form-control" name="usuario" access="false" id="usuario"></input>
                         </div>
                         <div class="formbuilder-text form-group field-telefono">
                             <label for="telefono" class="formbuilder-text-label">Teléfono de contacto:</label>
-                            <input type="text" placeholder="telefono" class="form-control" name="telefono" access="false" id="telefono"></input>
+                            <input type="text" placeholder="3014746531" class="form-control" name="telefono" access="false" id="telefono"></input>
                         </div>
                         <div class="formbuilder-text form-group field-correo">
                             <label for="correo" class="formbuilder-text-label">Correo electrónico:</label>
-                            <input type="text" placeholder="correo" class="form-control" name="correo" access="false" id="correo"></input>
+                            <input type="text" placeholder="luisgomez@gmail.com" class="form-control" name="correo" access="false" id="correo"></input>
                         </div>
                         <div class="formbuilder-text form-group field-habitacion">
                             <label for="habitacion" class="formbuilder-text-label">Habitación:</label>
-                            <input type="text" placeholder="deluxe" class="form-control" name="habitacion" access="false" id="habitacion"></input>
+                            <input type="text" placeholder="Deluxe Suite" class="form-control" name="habitacion" access="false" id="habitacion"></input>
                         </div>
                         <div class="formbuilder-text form-group field-capacidad">
                             <label for="capacidad" class="formbuilder-text-label">Capacidad:</label>
-                            <input type="text" placeholder="numero capacidad" class="form-control" name="capacidad" access="false" id="capacidad"></input>
+                            <input type="text" placeholder="3" class="form-control" name="capacidad" access="false" id="capacidad"></input>
                         </div>
                         <div class="formbuilder-text form-group field-text-1658882847121">
                             <label for="text-1658882847121" class="formbuilder-text-label">Camas:</label>
-                            <input type="text" placeholder="cantidad camas" class="form-control" name="text-1658882847121" access="false" id="text-1658882847121"></input>
+                            <input type="text" placeholder="2" class="form-control" name="text-1658882847121" access="false" id="text-1658882847121"></input>
                         </div>
                         <div class="formbuilder-textarea form-group field-caractersticas">
                             <label for="caractersticas" class="formbuilder-textarea-label">Descripción y características:</label>
-                            <textarea type="textarea" placeholder="detalles, key y si" class="form-control" name="caractersticas" access="false" id="caractersticas"></textarea>
+                            <textarea type="textarea" placeholder="Cama doble tipo Queen y una cama sencilla y baño privado donde podrás disfrutar de Direct Tv, internet, escritorio, caja de seguridad, servicio al cuarto, entre otros servicios." class="form-control" name="caractersticas" access="false" id="caractersticas"></textarea>
                         </div>
                         <div class="formbuilder-text form-group field-valornoche">
                             <label for="valornoche" class="formbuilder-text-label">Valor noche:</label>
-                            <input type="text" placeholder="$" class="form-control" name="valornoche" access="false" id="valornoche"></input>
+                            <input type="text" placeholder="$125000" class="form-control" name="valornoche" access="false" id="valornoche"></input>
                         </div>
                         <div class="formbuilder-text form-group field-fentrada">
                             <label for="fentrada" class="formbuilder-text-label">Fecha entrada:</label>
-                            <input type="text" placeholder="fentrada" class="form-control" name="fentrada" access="false" id="fentrada"></input>
+                            <input type="text" placeholder="2022-07-16" class="form-control" name="fentrada" access="false" id="fentrada"></input>
                         </div>
                         <div class="formbuilder-text form-group field-fsalida">
                             <label for="fsalida" class="formbuilder-text-label">Fecha salida:</label>
-                            <input type="text" placeholder="fsalida" class="form-control" name="fsalida" access="false" id="fsalida"></input>
+                            <input type="text" placeholder="2022-07-18" class="form-control" name="fsalida" access="false" id="fsalida"></input>
                         </div>
                         <div class="formbuilder-text form-group field-cantidadnoches">
                             <label for="cantidadnoches" class="formbuilder-text-label">Cantidad noches:</label>
-                            <input type="text" placeholder="cantidadnoches" class="form-control" name="cantidadnoches" access="false" id="cantidadnoches"></input>
+                            <input type="text" placeholder="2" class="form-control" name="cantidadnoches" access="false" id="cantidadnoches"></input>
                         </div>
                         <div class="formbuilder-text form-group field-totalreserva">
                             <label for="totalreserva" class="formbuilder-text-label">Total reserva:</label>
-                            <input type="text" placeholder="totalreserva" class="form-control" name="totalreserva" access="false" id="totalreserva"></input>
+                            <input type="text" placeholder="$250000" class="form-control" name="totalreserva" access="false" id="totalreserva"></input>
                         </div>
                     </div>
 
